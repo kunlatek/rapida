15f6ba788275d2ba2fec5d193694abbcf8b44eef
alexisbarros <apenasalexis@gmail.com>
2022-10-07 12:10:20 -0300

    feat: deep populate over populate

--- src/interfaces/form.ts
+++ src/interfaces/form.ts
@@ -188,6 +188,7 @@ export interface AutocompleteApiInterface {
   isDisabled?: boolean;
   isSelected?: boolean;
   todo?: string;
+  populate?: string[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
