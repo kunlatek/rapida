044b8198327366b21c516854c6b9af189f121342
Robson <robson.medeiros@opah.com.br>
2023-08-10 00:48:23 -0300

    chore: change testing

--- src/testing.ts
+++ src/testing.ts
@@ -1,4 +1,4 @@
-import { BackendFrameworkEnum, FrontendFrameworkEnum } from "./enums/main";
+import { FrontendFrameworkEnum } from "./enums/main";
 import { createProject } from "./index";
 
 
@@ -26,47 +26,56 @@ MONGO_URL=mongodb+srv://kunlatek:Kunlatek751@cluster0.b0pfr.mongodb.net/__DB_NAM
 DB=animation
 NODEMAILER_USER=administrativo@kunlatek.com
 NODEMAILER_PASS=yetyxhgzjzktuwef
-ADMIN_USERS=contato@kunlatek.com
+ADMIN_USERS=idrobsonmedeiros@gmail.com
 `;
 
 
 const cloneFrontendPath = "https://github.com/ryzzan/kunlatek-quickstart-invitation-and-permission";
 const cloneBackendPath = "https://github.com/kunlabori-teknologio/quickstart-api";
-const projectPath = "/home/ryzzan/Projects/Demo/animation";
+const projectPath = "/home/robsonpop/Projetos/dona-sara";
 
+import { COMPANY } from "../rapida-clients/clients/backoffice-sara/company";
+import { COMPANY_FORM } from "../rapida-clients/clients/backoffice-sara/company-form";
+import { COMPANY_TABLE } from "../rapida-clients/clients/backoffice-sara/company-table";
 
-import { ANIMATION } from "../rapida-clients/example/animation/animation";
-import { ANIMATION_FORM } from "../rapida-clients/example/animation/animation-form";
-import { ANIMATION_TABLE } from "../rapida-clients/example/animation/animation-table";
+import { LEAFLET } from "../rapida-clients/clients/backoffice-sara/leaflet";
+import { LEAFLET_FORM } from "../rapida-clients/clients/backoffice-sara/leaflet-form";
+import { LEAFLET_TABLE } from "../rapida-clients/clients/backoffice-sara/leaflet-table";
 
+import { PRODUCT } from "../rapida-clients/clients/backoffice-sara/product";
+import { PRODUCT_FORM } from "../rapida-clients/clients/backoffice-sara/product-form";
+import { PRODUCT_TABLE } from "../rapida-clients/clients/backoffice-sara/product-table";
 
-import { CHARACTER } from "../rapida-clients/example/animation/character";
-import { CHARACTER_FORM } from "../rapida-clients/example/animation/character-form";
-import { CHARACTER_TABLE } from "../rapida-clients/example/animation/character-table";
+import { RECALL } from "../rapida-clients/clients/backoffice-sara/recall";
+import { RECALL_FORM } from "../rapida-clients/clients/backoffice-sara/recall-form";
+import { RECALL_TABLE } from "../rapida-clients/clients/backoffice-sara/recall-table";
 
 
 const array = [
-  ANIMATION_FORM,
-  ANIMATION_TABLE,
-  ANIMATION,
+  COMPANY,
+  COMPANY_FORM,
+  COMPANY_TABLE,
 
+  LEAFLET,
+  LEAFLET_FORM,
+  LEAFLET_TABLE,
 
-  CHARACTER_FORM,
-  CHARACTER_TABLE,
-  CHARACTER,
+  PRODUCT,
+  PRODUCT_FORM,
+  PRODUCT_TABLE,
+
+  RECALL,
+  RECALL_FORM,
+  RECALL_TABLE,
 ];
 
-
 array.forEach((object) => {
-  object.backendFramework = BackendFrameworkEnum.Loopback;
+  // object.backendFramework = BackendFrameworkEnum.Loopback;
   object.frontendFramework = FrontendFrameworkEnum.Angular;
   object.projectPath = projectPath;
   object.cloneFrontendPath = cloneFrontendPath;
-  object.cloneBackendPath = cloneBackendPath;
   object.envFrontendDev = envFrontDev;
   object.envFrontendProd = envFrontProd;
-  object.envBackend = envBackend;
 });
-
 
 createProject(array);
